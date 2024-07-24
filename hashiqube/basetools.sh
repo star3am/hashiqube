97ef92885ad8fa3e19f2ffd0fa14ba0a90f33238
Riaan Nolan <riaan.nolan@gmail.com>
2024-07-24 20:05:20 +1000

    make apt-get a little more chatty so that user does not wait on nothing happening

--- hashiqube/basetools.sh
+++ hashiqube/basetools.sh
@@ -13,9 +13,9 @@ echo -e '\e[38;5;198m'"++++ "
 
 export DEBIAN_FRONTEND=noninteractive
 export PATH=$PATH:/root/.local/bin
-sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes update -qq -o Acquire::CompressionTypes::Order::=gz < /dev/null > /dev/null
-sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes upgrade -qq < /dev/null > /dev/null
-sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install -qq swapspace jq curl unzip software-properties-common bzip2 git make python3 python3-pip python3-dev python3-venv python3-virtualenv python3-passlib golang-go apt-utils ntp update-motd toilet figlet nano iputils-ping dnsutils iptables net-tools telnet mc iproute2 psmisc < /dev/null > /dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes update -q -o Dpkg::Progress-Fancy="1" -o Acquire::CompressionTypes::Order::=gz # < /dev/null > /dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes upgrade -q -o Dpkg::Progress-Fancy="1" # < /dev/null > /dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install -q -o Dpkg::Progress-Fancy="1" swapspace jq curl unzip software-properties-common bzip2 git make python3 python3-pip python3-dev python3-venv python3-virtualenv python3-passlib golang-go apt-utils ntp update-motd toilet figlet nano iputils-ping dnsutils iptables net-tools telnet mc iproute2 psmisc # < /dev/null > /dev/null
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1 --force
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1 --force
 systemctl restart swapspace
